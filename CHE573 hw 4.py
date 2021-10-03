1d71fb5e97e5ea245a8e8ba02466ab1e1bc1ce19
Travis Czechorski <tjczec01@gmail.com>
2021-10-03 16:28:08 -0400

    Add files via upload

---
+++ CHE573 hw 4.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Sun Oct  3 16:00:56 2021
+
+@author: Travis J Czechorski
+
+Github: https://github.com/tjczec01
+
+Email:   travis.czechorski@maine.edu
+         tjczec01@gmail.com
+        
+Advisor: thomas.schwartz@maine.edu
+       
+Github:  https://github.com/tjczec01 
+         https://github.com/TravisCzechorskiUMaine
+
+"""
+
+import pandas as pd
+import math as mt
+
+df = pd.read_excel(r'C:\Users\tjcze\Downloads\StandardDevLJ.xlsx')
+energy_list = df['-205.03321714472892'].tolist()
+# print(energy_list)
+
+def std_dev(pop_list):
+    N = len(pop_list)
+    AVG = sum(pop_list)/len(pop_list)
+    subtracted_values = [abs(i - AVG) for i in pop_list]
+    numerator = sum(subtracted_values)
+    final_value = mt.sqrt(numerator/N)
+    return final_value
+
+answer = std_dev(energy_list)
+print(answer)
+    
